f41b05d0bd2239bf9b18e6c10ef4c7cb15251adf
Zero <minatouzumakin001@gmail.com>
2018-06-08 08:10:56 +0545

    code refactor

--- Game/Source/game_entry.cpp
+++ Game/Source/game_entry.cpp
@@ -1,4 +1,5 @@
 #include "s00nya.h"
+#include "player_animated.h"
 #include "player.h"
 
 using namespace s00nya;
@@ -12,36 +13,18 @@ public:
 	MyGame() :
 		Game2D("Example Game", 800, 600, Samples::SAMPLE_DOUBLE)
 	{
-		SetClearColor(0.0f, 0.0f, 0.01f);
+		SetClearColor(0.0f, 0.0f, 0.0f);
 
 		resource->LoadRawImage("./Resources/window_test_icon.png", "WolfIcon");
 		window->SetIcon(resource->GetRawImage("WolfIcon"));
 
 		resource->LoadSpriteSheet("./Resources/player.png", 4, 4, "Player");
 
-		Dimension dimension(220.0f, 280.0f);
-		Collider2D col;
-
-		Transform2D playerTrans;
-		playerTrans.position = { 200.0f, 200.0f, 0.0f };
-		playerTrans.rotation = 0.0f;
-		playerTrans.scale = { 0.25f, 0.25f };
-
-		Transform2D another;
-		another.position = { 0.0f, 0.0f, 0.0f };
-		another.rotation = 0.0f;
-		another.scale = { 0.25f, 0.25f };
-
-		Material playerMat;
-		playerMat.diffuse = "Player";
-		playerMat.frame = 0;
-		playerMat.type = Material::Type::CLAMP_TO_EDGE;
-
 		level1 = PushScene(new Scene({ -1.0f, 1.0f }, { 0.0f, 800.0f }, { 600.0f, 0.0f }));
 		ActivateScene(level1);
 
-		AddGameObject2D(new Player(playerTrans, dimension, playerMat, col), "Hero");
-		AddGameObject2D(new GameObject2D(another, dimension, playerMat, col), "Another");
+		AddGameObject2D(new PlayerAnimated(), "Hero");
+		AddGameObject2D(new Player(), "Another");
 	}
 
 };
--- Game/Source/player_animated.h
+++ Game/Source/player_animated.h
@@ -4,18 +4,18 @@
 
 using namespace s00nya;
 
-class Player : public GameObject2D
+class PlayerAnimated : public GameObject2D
 {
 
 public:
-	Input & input;
+	Input& input;
 	Timer& timer;
 	Float speed = 200.0f;
 	Float lastTime = 0.0f;
 
-	Player() :
+	PlayerAnimated() :
 		GameObject2D(
-			Transform2D({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, 0.0f),
+			Transform2D({ 200.0f, 200.0f, 0.0f }, { 1.0f, 1.0f }, 0.0f), 
 			Dimension(50.0f, 80.0f),
 			Material()),
 		input(Game2D::GetInput()),
@@ -26,19 +26,32 @@ public:
 		this->material.type = Material::Type::CLAMP_TO_EDGE;
 	}
 
+	void FixedUpdate() override
+	{
+		if ((timer.ElaspedTime() - lastTime) > 1.0f)
+		{
+			lastTime = timer.ElaspedTime();
+			this->material.frame++;
+			if (this->material.frame > 15) material.frame = 0;
+		}
+	}
+
 	void Update() override
 	{
-		if (input.Held(Keys::KEY_A))
+		if(input.Held(Keys::DIRECTIONAL_LEFT))
 			transform.position.x -= (timer.DeltaTime()*speed);
-
-		if (input.Held(Keys::KEY_D))
+	
+		if (input.Held(Keys::DIRECTIONAL_RIGHT))
 			transform.position.x += (timer.DeltaTime()*speed);
-
-		if (input.Held(Keys::KEY_W))
+		
+		if (input.Held(Keys::DIRECTIONAL_UP))
 			transform.position.y += (timer.DeltaTime()*speed);
-
-		if (input.Held(Keys::KEY_S))
+		
+		if (input.Held(Keys::DIRECTIONAL_DOWN))
 			transform.position.y -= (timer.DeltaTime()*speed);
+		
+		if (input.Pressed(Keys::ESCAPE))
+			Game2D::GetEventManager().Push(Events::SYSTEM, "ShutDown");
 	}
 
 };
